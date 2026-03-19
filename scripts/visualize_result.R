2c15795f67f1b88240e98ea357510e8f2e4ad5f8
tonyu0 <tonyu.grapefruit@gmail.com>
2026-03-19 18:39:00 +0900

    cleanup scripts/visualize_result.R

--- scripts/visualize_result.R
+++ scripts/visualize_result.R
@@ -1,25 +1,27 @@
 if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-BiocManager::install(c("Gviz", "GenomicRanges", "rtracklayer", "RColorBrewer"), ask = F)
+BiocManager::install(c("Gviz", "GenomicRanges", "rtracklayer", "RColorBrewer", "TxDb.Hsapiens.UCSC.hg38.knownGene"), ask = F)
 
 library(Gviz)
 library(GenomicRanges)
 library(rtracklayer)
 library(ggplot2)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 
 # ========== 1. Scatter plot with ggplot ==========
-{ # Scatter plot of expression and methylation of extracted gene
+draw_expression_methlylation_scatter <- function() { # Scatter plot of expression and methylation of extracted gene
   dmr_regulated_genes <-  read.csv("DMRs_regulated_genes.csv")
   
   png("expression_methylation_scatter.png", width = 1000, height = 1200, res = 150)
-  ggplot(data = dmr_regulated_genes, aes(x = score, y = log2FoldChange, color = SYMBOL)) +
+  p <- ggplot(data = dmr_regulated_genes, aes(x = score, y = log2FoldChange, color = SYMBOL)) +
     geom_point() +
     labs(title = "Expression level vs. Methylation level (Liver vs Primary)", 
          x = "Methylation level (diff %)", y = "Expression level (Log2 Fold Change)", color = "Gene symbol")
+  print(p) # this is required when executed from function
   dev.off()
 }
 
 # ========== 2. Visualize tracks of gene model (annotation) with Gviz ==========
-{
+draw_gene_with_gviz <- function() {
   # DMR: 155079868,155080372
   # Gene: 155079932,155183678,
   target_chr <- "chr3" 
@@ -28,10 +30,10 @@ library(ggplot2)
   gen <- "hg38"
   
   axis_track <- GenomeAxisTrack()
-  gtf_data <- import("Homo_sapiens.GRCh38.112.gtf")
   
-  gene_track <- GeneRegionTrack(gtf_data, name = "Gene",
-                                genome = gen, chromosome = target_chr, 
+  txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+  gene_track <- GeneRegionTrack(txdb, name = "Gene",
+                                genome = gen, chromosome = target_chr,
                                 transcriptAnnotation = "symbol", background.title = "brown")
   
   # ========== CGI track ==========
@@ -46,25 +48,32 @@ library(ggplot2)
   dmrs_track   <- AnnotationTrack(dmrs, name = "DMR", fill = "red", alpha = 0.8)
   
   # Use data only from #314
-  options(ucscChromosomeNames=FALSE) # bismark bedGraph somehow output error
+#  options(ucscChromosomeNames=FALSE) # bismark bedGraph somehow output error
   # ========== RNA-seq track (use bigWig from star_salmon) ==========
-  liver_expression_track <- DataTrack(range = "rnaseq_output/star_salmon/bigwig/SRX17589504.forward.bigWig", name = "Liver Meth", 
-                                      type = "gradient", col = "darkred", ylim = c(0, 1))
-  primary_expression_track <- DataTrack(range = "rnaseq_output/star_salmon/bigwig/SRX17589513.forward.bigWig" , name = "Primary Meth", 
-                                        type = "gradient", col = "darkblue", ylim = c(0, 1))
+#  liver_expression_track <- DataTrack(range = "rnaseq_output/star_salmon/bigwig/SRX17589504.forward.bigWig", name = "Liver Meth", 
+#                                      type = "gradient", col = "darkred", ylim = c(0, 1))
+#  primary_expression_track <- DataTrack(range = "rnaseq_output/star_salmon/bigwig/SRX17589513.forward.bigWig" , name = "Primary Meth", 
+#                                        type = "gradient", col = "darkblue", ylim = c(0, 1))
   # ========== Methylation track (use bedGraph from bismark) ==========
-  liver_methylation_track <- DataTrack(range = "methylseq_output/bismark/methylation_calls/bedGraph/SRX17589500_trimmed_bismark_bt2.bedGraph.gz", name = "Liver RNA", 
-                                       type = "histogram", fill = "salmon", col = "salmon")
-  primary_methylation_track <- DataTrack(range = "methylseq_output/bismark/methylation_calls/bedGraph/SRX17589490_trimmed_bismark_bt2.bedGraph.gz", name = "Primary RNA", 
-                                         type = "histogram", fill = "skyblue", col = "skyblue", ucscChro)
+#  liver_methylation_track <- DataTrack(range = "methylseq_output/bismark/methylation_calls/bedGraph/SRX17589500_trimmed_bismark_bt2.bedGraph.gz", name = "Liver RNA", 
+#                                       type = "histogram", fill = "salmon", col = "salmon")
+#  primary_methylation_track <- DataTrack(range = "methylseq_output/bismark/methylation_calls/bedGraph/SRX17589490_trimmed_bismark_bt2.bedGraph.gz", name = "Primary RNA", 
+#                                         type = "histogram", fill = "skyblue", col = "skyblue", ucscChro)
   # ========== Plot ==========
   png("MME_integration_plot.png", width = 1000, height = 1200, res = 150)
   plotTracks(list(axis_track, 
-                  liver_expression_track, liver_methylation_track, 
-                  primary_expression_track, primary_methylation_track, 
-                  dmr_track, shore_track, cgi_track, 
+#                  liver_expression_track, liver_methylation_track, 
+#                  primary_expression_track, primary_methylation_track, 
+                  dmrs_track, shore_track, cgi_track, 
                   from = target_start, to = target_end, 
                   gene_track),
              main = "Epigenetic Regulation of MME (ENSG00000196549)", transcriptAnnotation = "symbol")
   dev.off()
+}
+
+{ # main
+  print("========== Drawing expression_methylation_scatter.png ==========")
+  draw_expression_methlylation_scatter()
+  print("========== Drawing MME_integration_plot.png ==========")
+  draw_gene_with_gviz()
 }
